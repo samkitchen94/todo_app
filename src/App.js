08f3d7ccf87b2f0b2333d6d59e9ad166bd1ee44e
Sam Kitchen <samkitchen94@gmail.com>
2019-08-28 16:45:31 +0100

    now get requests from api appear as todos using setState

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
 import uuid from 'uuid';
-
+import axios from 'axios';
 
 import './App.css';
-import axios from 'axios';
+
 
 class App extends Component {
   state = {
@@ -17,8 +17,8 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/todos')
-      .then(res => console.log(res.data))
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=20')
+      .then(res => this.setState({ todos: res.data }))
   }
 
 // Mark as complete
