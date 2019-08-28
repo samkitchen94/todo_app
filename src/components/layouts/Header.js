26f8cb65d50f1156a611ce97b7400cbadfb75e25
Sam Kitchen <samkitchen94@gmail.com>
2019-08-28 15:45:40 +0100

    links added for about and home pages using Link import from react router

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header style={headerStyle}>
       <h1> Todo List </h1>
+      <Link to='/'> Home </Link> | 
+      <Link to='/about'> About </Link>
     </header>
   )
 }
